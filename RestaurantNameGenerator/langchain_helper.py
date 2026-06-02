95238b7a29836d6c585dc59ab1ad610c7c46e558
Chibuike Dominion <140020378+Dominionai@users.noreply.github.com>
2026-06-02 21:39:27 +0100

    Add imports for langchain and secret key

--- RestaurantNameGenerator/langchain_helper.py
+++ RestaurantNameGenerator/langchain_helper.py
@@ -1,4 +1,7 @@
-
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from secret_key import groq_api_key 
 
 import os
 os.environ["GROQ_API_KEY"] = groq_api_key
